dd3200f5c0edab3fb37d55a94e0c44b84fad524e
rkkmkkfx(Baranov Sergei) <baranovs-v@mail.ru>
2018-03-27 03:53:14 +0800

    Done!

--- vk_app.py
+++ vk_app.py
@@ -1,16 +1,34 @@
 import requests
+from urllib.parse import urlencode
 
-token = 'd43e382f925bfd9db775fac4f46980ede6f3c6af139fb9e7aa2f850ed0ba12d81f9046a62b1d420da21fe'
+APP_ID = 6380141
+AUTHORIZE_URL = 'https://oauth.vk.com/authorize'
 
-first_user = input('First User ')
-second_user = input('Second User ')
-
-params = {
-    'source_uid': first_user,
-    'target_uid': second_user,
-    'access_token': token
+auth_params = {
+    'client_id': APP_ID,
+    'redirect_url': 'https://oauth.vk.com/blank.html',
+    'display': 'popup',
+    'scope': 'friends',
+    'response_type': 'token'
 }
 
-res = requests.get('https://api.vk.com/method/friends.getMutual', params)
+print('?'.join((AUTHORIZE_URL, urlencode(auth_params))))
 
-print(res.json())
+token = '7589c9f1f059c7b33efb70a74210aff77688221c65c2310d27afd511a7d6659b26179cbdb36c6ffe9de25'
+
+FIRST_ID = 1
+SECOND_ID = 2
+
+def get_mutual(first_user, second_user):
+    params = {
+        'source_uid': first_user,
+        'target_uid': second_user,
+        'access_token': token,
+        'v': '5.73'
+    }
+
+    return requests.get('https://api.vk.com/method/friends.getMutual', params).json()['response']
+
+
+for user in get_mutual(FIRST_ID, SECOND_ID):
+    print('https://vk.com/id{}'.format(user))
